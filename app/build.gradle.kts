bb458e18d2c2bf09566594b1dd81329c751d9f77
GuranshS <guranshsingh273@gmail.com>
2025-10-16 13:38:15 +1100

    doc: added dokka dependency for documentation generation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,9 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.dokka)
 }
+
 
 android {
     namespace = "com.example.tabcasterclient1"

6f329e9df4053b8900061a6f822225489fa8aa0d
Jazztache <jazanaterz@gmail.com>
2025-11-13 09:52:05 +1100

    Prefs Manager
    This will allow users to store the last IP that they connected to.

---
+++ app/src/main/java/com/example/tabcasterclient1/PrefsManager.kt
@@ -0,0 +1,24 @@
+package com.example.tabcasterclient1
+
+import android.content.SharedPreferences
+import androidx.appcompat.app.AppCompatActivity
+
+
+class PrefsManager(private val activity: AppCompatActivity) {
+
+    private lateinit var lastConnectedIP : String
+
+    protected fun setLastIP(lastValidIP: String) {
+        lastConnectedIP = lastValidIP
+        // Save IP Address To Preferences
+        val settings: SharedPreferences = activity.getSharedPreferences("lastConnectedIP", 0)
+        val editor = settings.edit()
+        editor.putString("lastConnectedIP", lastValidIP)
+        editor.apply()
+    }
+
+    protected fun getLastIP(){
+        val settings: SharedPreferences = activity.getSharedPreferences("lastConnectedIP", 0)
+        val homeScore = settings.getString("homeScore", "")
+    }
+}

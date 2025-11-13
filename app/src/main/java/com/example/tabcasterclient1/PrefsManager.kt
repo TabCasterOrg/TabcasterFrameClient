380f033745597569a11078ef19bb1cb0a705120e
Jazztache <jazanaterz@gmail.com>
2025-11-13 11:14:08 +1100

    Prefs Manager Now Uses Singleton Structure

--- app/src/main/java/com/example/tabcasterclient1/PrefsManager.kt
+++ app/src/main/java/com/example/tabcasterclient1/PrefsManager.kt
@@ -1,24 +1,33 @@
 package com.example.tabcasterclient1
 
+import android.content.Context
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 
 
-class PrefsManager(private val activity: AppCompatActivity) {
+// This is a singleton, and uses context injection
+class PrefsManager private constructor(context: Context) {
+        private val prefs = context.applicationContext.getSharedPreferences("app_prefs", Context.MODE_PRIVATE)
 
-    private lateinit var lastConnectedIP : String
+        private lateinit var lastConnectedIP : String
 
-    private fun setLastIP(lastValidIP: String) {
-        lastConnectedIP = lastValidIP
-        // Save IP Address To Preferences
-        val settings: SharedPreferences = activity.getSharedPreferences("lastConnectedIP", 0)
-        val editor = settings.edit()
-        editor.putString("lastConnectedIP", lastValidIP)
-        editor.apply()
-    }
+        fun setLastIP(lastValidIP: String) {
+            lastConnectedIP = lastValidIP
+            // Save IP Address To Preferences
+            prefs.edit().putString("lastConnectedIP", lastValidIP).apply() // Get the editor, put the string in and apply all in one line.
+        }
 
-    private fun getLastIP(){
-        val settings: SharedPreferences = activity.getSharedPreferences("lastConnectedIP", 0)
-        val homeScore = settings.getString("homeScore", "")
+        fun getLastIP(): String? = prefs.getString("lastConnectedIP", null)
+
+    companion object {
+        @Volatile
+
+        private var INSTANCE: PrefsManager? = null
+
+        fun getInstance(context: Context): PrefsManager {
+            return INSTANCE ?: synchronized(this) {
+                INSTANCE ?: PrefsManager(context).also { INSTANCE = it }
+            }
+        }
     }
 }
